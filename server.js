a27f45643518acb0286faaf91a1a647a22b8bb55
Jean-Daniel Liwanga <jeandaniel.liwanga@gmail.com>
2024-01-28 09:01:06 -0500

    Minor change for CORS

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL, //'http://localhost:3000'
+  origin: process.env.FRONTEND_URL, //'http://localhost:3000
   credentials: true
 }));
 
